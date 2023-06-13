da944827705f89ef2cf0b66d441cd2d89be66fd8
Vitor Falcao <joaofalcao.33@outlook.com>
2023-06-13 13:48:25 -0300

    up to vercel

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use((req,res,next) =>{
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   // app.use(cors());
   next();
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -36,51 +36,24 @@ router.post("/", async (req, res) => {
     return res.status(500).json({ msg: "algum erro" });
   }
 });
-// UPDATE password e/ou name exigindo nova senha
-// confere senha antes de nova senha
-router.patch("/update/:id", async (req, res) => {
-  const id = req.params.id;
-  const { name, email, password, newPassword } = req.body;
-  const userById = await UserModel.findOne({ _id: id });
-  const checkPassword = await bcrypt.compare(password, userById.password);
-  const salt = await bcrypt.genSalt(12); // dificultador da senha
-  const passwordHash = await bcrypt.hash(newPassword, salt); // novo password vai com hash
-  if (checkPassword && userById) {
-  if (password !== newPassword) {
-      // se password confere e achou id pode atualizar o novopass
-      const person = {
-        name,
-        email,
-        password: passwordHash,
-      };
-      const updatedPerson = await UserModel.updateOne({ _id: id }, person);
-      res.status(200).json({ msg: "banco atualizou name email e newpassword" });
-    } else {
-      res.status(401).json({ msg: "password é igual" });
-    }
-  }
-});
-
 // DELETE by email, find id and delete it
 // Compara senha vindo de bcrypt
-router.delete("/deletar", async (req, res) => {
-  const { email, password } = req.body;
-  const userByEmail = await UserModel.findOne({ email: email });
-  if(userByEmail){
-    const checkPassword = await bcrypt.compare(password, userByEmail.password);
-    if (checkPassword ) {
-      try {
-        await UserModel.deleteOne({ _id: userByEmail.id });
-        return res.status(200).json({ msg: "deletou" });
-      } catch (error) {
-        return res.status(401).json({ msg: "nao deletou" });
-      }
-    }else{
-      return res.status(401).json({msg: "linha 78"})
+router.delete("/deletar/:id", async (req, res) => {
+  const id = req.params.id
+  const userById = await UserModel.findOne({ _id: id });
+  //  const checkPassword = await bcrypt.compare(password, userById.password);
+  if(userById){
+    console.log(userById)
+    try {
+      await UserModel.deleteOne({ _id: userById.id });
+      return res.status(200).json({ msg: "deletou" });
+    } catch (error) {
+      return res.status(401).json({ msg: "nao deletou" });
     }
   }else{
-    return res.status(401).json({msg: "email nao encontrado"})
+    res.status(400).json({msg: "não encontrou id"})
   }
+  
 });
 
 // Private route
@@ -91,14 +64,13 @@ router.get("/private/:id", checkToken, async (req, res) => {
   if (!user) {
     return res.status(404).json({ msg: "usuario nao encontrado" });
   }
-  res.status(200).json({ user });
+  res.status(200).json({ msg: "Private route OK" });
 });
 
 // MIDDLEWARE
-function checkToken(req, res, next) {
+async function checkToken (req, res, next) {
     // este é um middleware
     const authHeader = req.headers["authorization"]; // PEGA da req.headers indice authorization
-    console.log(authHeader)
     const token = authHeader && authHeader.split(" ")[1];
     if (!token) {
       return res.status(401).json({ msg: "token invalido" });
@@ -141,6 +113,34 @@ router.post("/auth/login", async (req, res) => {
     } catch (error) {
       return res.status(500).json({ msg: "token invalido" });
     }
+  }
+});
+// UPDATE password e/ou name exigindo nova senha
+// confere senha antes de nova senha
+router.patch("/update/:id", async (req, res) => {
+  const id = req.params.id;
+  if(!undefined){
+    const { name, email, password, newPassword } = req.body;
+    const userById = await UserModel.findOne({ _id: id });
+    const checkPassword = await bcrypt.compare(password, userById.password);
+    const salt = await bcrypt.genSalt(12); // dificultador da senha
+    const passwordHash = await bcrypt.hash(newPassword, salt); // novo password vai com hash
+    if (checkPassword && userById) {
+    if (password !== newPassword) {
+        // se password confere e achou id pode atualizar o novopass
+        const person = {
+          name,
+          email,
+          password: passwordHash,
+        };
+        const updatedPerson = await UserModel.updateOne({ _id: id }, person);
+        res.status(200).json({ msg: "banco atualizou name email e newpassword" });
+      } else {
+        res.status(401).json({ msg: "password é igual" });
+      }
+    }
+  }else{
+    res.status(401).json({ msg: "chamar id primeiro" });
   }
 });
 
